d5dca07493930a02156f077c1f5b940debdcb4e1
Jenesis Merriman <jenesishbm@berkeley.edu>
2023-09-18 15:03:49 +0100

    exercise 4

--- example.rb
+++ example.rb
@@ -31,3 +31,17 @@ puts movies[:hereditary]
 puts movies[:conjuring]
 puts movies[:feed]
 puts movies[:frozen]
+
+# 4. Use the dates from the previous example and store them in an array. Then make your program output the same thing as exercise 3.
+
+#my attempt
+array = [movies[:hulk], movies[:hereditary], movies[:conjuring], movies[:feed], movies[:frozen]]
+
+#ls solution
+years = [2001, 2019, 2018, 2005, 2016]
+
+puts years[0]
+puts years[1]
+puts years[2]
+puts years[3]
+puts years[4]
