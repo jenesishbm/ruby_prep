126b8f7d7ca8ac5afd7018d85ddf6cfb01378e16
Jenesis Merriman <jenesishbm@berkeley.edu>
2023-09-18 14:05:47 +0100

    exercise 1

--- example.rb
+++ example.rb
@@ -1,1 +1,9 @@
-puts 'hello world'
+# The Basics
+
+# 1. Add two strings together that, when concatenated, return your first and last name as your full name in one string.
+first_name = "Jenesis"
+last_name = "Merriman"
+
+puts first_name + " " + last_name
+
+# Use the modulo operator, division, or a combination of both to take a 4 digit number and find the digit in the: 1) thousands place 2) hundreds place 3) tens place 4) ones place
