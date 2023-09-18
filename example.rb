b49eb2247d8ce7b0a75403f7b451d6f4e09fd553
Jenesis Merriman <jenesishbm@berkeley.edu>
2023-09-18 14:18:46 +0100

    exercise 2

--- example.rb
+++ example.rb
@@ -6,4 +6,13 @@ last_name = "Merriman"
 
 puts first_name + " " + last_name
 
-# Use the modulo operator, division, or a combination of both to take a 4 digit number and find the digit in the: 1) thousands place 2) hundreds place 3) tens place 4) ones place
+# 2. Use the modulo operator, division, or a combination of both to take a 4 digit number and find the digit in the: 1) thousands place 2) hundreds place 3) tens place 4) ones place
+thousands = 4936 / 1000
+hundreds = 4936 % 1000 / 100
+tens = 4936 % 100 / 10
+ones = 4936 % 10
+
+puts "thousands: #{thousands}"
+puts "hundreds: #{hundreds}"
+puts "tens: #{tens}"
+puts "ones: #{ones}"
