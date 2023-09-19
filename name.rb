5cc952dfaf555a0486040dc4074d0b7c50834926
Jenesis Merriman <jenesishbm@berkeley.edu>
2023-09-19 13:50:10 +0100

    first and last name

--- name.rb
+++ name.rb
@@ -1,9 +1,13 @@
 # VARIABLES
 
 # 1. Write a program called name.rb that asks the user to type in their name and then prints out a greeting message with their name included.
-puts "What is your name?"
-name = gets.chomp
-puts "Hello " + name
+# 4. Modify name.rb again so that it first asks the user for their first name, saves it into a variable, and then does the same for the last name. Then outputs their full name all at once.
+puts "What is your first name?"
+first_name = gets.chomp
+puts "What is your last name?"
+last_name = gets.chomp
+puts "Hello " + first_name + " " + last_name
 
+# 3. Add another section onto name.rb that prints the name of the user 10 times.
 puts "Here is your name 10 times:"
-10.times { puts name }
+10.times { puts first_name + " " + last_name }
