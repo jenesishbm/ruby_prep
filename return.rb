f287eab85722a56c9c8772ef35e5703917aad280
Jenesis Merriman <jenesishbm@berkeley.edu>
2023-09-19 15:16:01 +0100

    update

---
+++ return.rb
@@ -0,0 +1,10 @@
+# METHODS
+
+def add_three(number) #define add_three method
+  number + 3
+end
+
+returned_value = add_three(4)
+puts returned_value
+
+# Ruby methods ALWAYS return the evaluated result of the last line of the expression unless an explicit return comes before it.
