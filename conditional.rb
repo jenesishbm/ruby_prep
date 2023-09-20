f9144deea5c8826d77e535368ea3cc6058fb7c40
Jenesis Merriman <jenesishbm@berkeley.edu>
2023-09-20 11:55:18 +0100

    add conditional.rb file

---
+++ conditional.rb
@@ -0,0 +1,11 @@
+# FLOW CONTROL
+
+puts "Put in a number"
+a = gets.chomp.to_i
+
+if a == 3
+  puts "Your number is 3"
+elsif a == 4
+  puts "Your number is 4"
+else puts "Your number is neither 3 nor 4"
+end
