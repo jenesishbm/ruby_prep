fdb65e2046316644beccfef3bdf15e13e93203fb
Jenesis Merriman <jenesishbm@berkeley.edu>
2023-09-20 13:48:27 +0100

    add last few folders

---
+++ loops_iterators/conditional_loop.rb
@@ -0,0 +1,24 @@
+# conditional loop
+
+i = 0
+loop do
+  i = i + 2
+  puts i
+  if i == 10
+    break
+  end
+end
+
+# conditional loop using next to skip iteration
+
+i = 0
+loop do
+  i = i + 2
+  if i == 4
+    next        # skip rest of the code in this iteration
+  end
+  puts i
+  if i == 10
+    break
+  end
+end
