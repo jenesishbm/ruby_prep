acdd3c9721f57d8e075363f337c030ef1375a14c
Jenesis Merriman <jenesishbm@berkeley.edu>
2023-09-27 15:09:14 +0100

    add comments to code

--- loops_iterators/conditional_loop.rb
+++ loops_iterators/conditional_loop.rb
@@ -32,7 +32,7 @@ while x <= 10
     puts x
   end
   x += 1
-end
+end # retuns 1, 3, 5, 7, 9
 
 # conditional_while_loop_with_next.rb
 
@@ -46,4 +46,17 @@ while x <= 10
     puts x
   end
   x += 1
-end
+end # skips 3 and returns 1, 5, 7, 9
+
+# conditional_while_loop_with_break.rb
+
+x = 0
+
+while x <= 10
+  if x == 7
+    break
+  elsif x.odd?
+    puts x
+  end
+  x += 1
+end # quits loop once x is 7 and returns 1, 3, 5
