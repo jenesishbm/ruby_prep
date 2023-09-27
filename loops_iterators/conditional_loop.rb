ebaca6f232753f909a8ebb88e887584c695d7892
Jenesis Merriman <jenesishbm@berkeley.edu>
2023-09-27 15:03:32 +0100

    loops and iterator example questions

--- loops_iterators/conditional_loop.rb
+++ loops_iterators/conditional_loop.rb
@@ -22,3 +22,28 @@ loop do
     break
   end
 end
+
+# conditional_while_loop.rb
+
+x = 0
+
+while x <= 10
+  if x.odd?
+    puts x
+  end
+  x += 1
+end
+
+# conditional_while_loop_with_next.rb
+
+x = 0
+
+while x <= 10
+  if x == 3
+    x += 1
+    next
+  elsif x.odd?
+    puts x
+  end
+  x += 1
+end
