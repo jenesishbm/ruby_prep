19bb27c3a754ec394cece8c34c6d381dc2b18633
Jenesis Merriman <jenesishbm@berkeley.edu>
2023-10-03 15:39:29 +0100

    add user input exercises and test.rb to isolate and run specific programs

--- exercises/exercises2.rb
+++ exercises/exercises2.rb
@@ -301,5 +301,213 @@ end
 count = 1
 
 loop do
+  puts "#{count} is odd!" if count.odd?
+  puts "#{count} is even!" if count.even?
   count += 1
+  break if count > 5 # would be break if count == 5 IF this came before the count +=1 line
 end
+
+# NOTE: always be aware of where you are incrementing your count variable and where you are enforcing the break/until/while statement.
+
+# 2. Modify the following code so that the loop stops if number is equal to or between 0 and 10.
+
+loop do
+  number = rand(100)
+  puts number
+  break if number <= 10 # rand already has a lower limit inclusive of 0. Another option is break if number.between?(0, 10)
+end
+
+# 3. Write an if/else statement that executes some code if the process_the_loop variable is true and some other code when process_the_loop is false. Specifically:
+
+process_the_loop = [true, false].sample
+
+if process_the_loop
+  loop do
+    puts "The loop was processed!"
+    break
+  end
+else 
+  puts "The loop was not processed!"
+end
+
+# 4. The code below asks the user "What does 2 + 2 equal?" and uses #gets to retrieve the user's answer. Modify the code so "That's correct!" is printed and the loop stops when the user's answer equals 4. Print "Wrong answer. Try again!" if the user's answer doesn't equal 4.
+
+loop do
+  puts 'What does 2 + 2 equal?'
+  answer = gets.chomp.to_i
+  if answer == 4 #added
+    puts "That's correct!" #added
+    break #added
+  else #added
+    puts "Wrong answer. Try again!" #added
+  end #added
+end
+
+# 5. Modify the code below so that the user's input gets added to the numbers array. Stop the loop when the array contains 5 numbers.
+
+numbers = []
+
+loop do
+  puts 'Enter any number:'
+  input = gets.chomp.to_i
+  numbers << input # can also use numbers.push(input)
+  break if numbers.length == 5
+end
+puts numbers
+
+# 6. Given the array below, use loop to remove and print each name from first to last. Stop the loop once names doesn't contain any more elements.
+
+names = ['Sally', 'Joe', 'Lisa', 'Henry']
+
+loop do
+  puts names[0] #prints the first element in an array
+  names.shift # deletes the first element in an array
+  break if names.length == 0
+end
+
+# LS alternative solution:
+
+loop do
+  puts names.shift # .shift() prints first element and deletes it
+  break if names.empty? # checks if array is empty (true or false)
+end
+
+# Extra practice: in reverse
+loop do
+  puts names.pop # .pop() prints last element and deletes it
+  break if names.empty?
+end
+
+# 7. The method below counts from 0 to 4. Modify the block so that it prints the current number and stops iterating when the current number equals 2.
+
+5.times do |index|
+  puts index
+  break if index == 2
+end
+
+# 8. Using next, modify the code below so that it only prints positive integers that are even.
+
+number = 0
+
+until number == 10
+  number += 1
+  next if number.odd? # skips code below next if number is odd
+  puts number
+end
+
+# 9. The following code increments number_a and number_b by either 0 or 1. loop is used so that the variables can be incremented more than once, however, break stops the loop after the first iteration. Use next to modify the code so that the loop iterates until either number_a or number_b equals 5. Print "5 was reached!" before breaking out of the loop.
+
+number_a = 0
+number_b = 0
+
+loop do
+  number_a += rand(2)
+  number_b += rand(2)
+  next unless number_a == 5 || number_b == 5 # next UNLESS, not next IF. Skips code below UNLESS number = 5
+  puts "5 was reached!"
+  break
+end
+
+# 10. Given the code below, use a while loop to print "Hello!" twice.
+
+def greeting
+  puts 'Hello!'
+end
+
+number_of_greetings = 2
+
+while number_of_greetings > 0
+  greeting
+  number_of_greetings -= 1
+end
+
+## USER INPUT
+
+# 1. Write a program that asks the user to type something in, after which your program should simply display what was entered.
+
+puts ">> Type anything you want"
+answer = gets.chomp
+puts answer
+
+# 2. Write a program that asks the user for their age in years, and then converts that age to months.
+puts ">> What is your age in years?"
+age = gets.chomp.to_i
+puts "You are #{age*12} months old"
+
+# 3. Write a program that asks the user whether they want the program to print "something", then print it if the user enters y. Otherwise, print nothing.
+
+puts ">> Do you want me to print something? (y/n)"
+choice = gets.chomp.downcase
+puts "something" if choice == "y"
+
+# 4. Modify your program so it prints an error message for any inputs that aren't y or n, and then asks you to try again. Keep asking for a response until you receive a valid y or n response.
+
+loop do
+  puts ">> Do you want me to print something? (y/n)"
+  choice = gets.chomp.downcase
+  if choice == "y"
+    puts "something"
+    break
+  elsif choice == "n"
+    break
+  else
+    puts "Error: Answer must be y or n. Try again."
+  end
+end
+
+# LS solution:
+
+choice = nil #define choice outside of loop so we can use it in last line
+
+loop do
+  puts '>> Do you want me to print something? (y/n)'
+  choice = gets.chomp.downcase
+  break if ["y", "n"].include?(choice) #break if choice is included in the array ["y", "n"]
+  puts '>> Invalid input! Please enter y or n'
+end
+puts 'something' if choice == 'y'
+
+# 5. Write a program that prints 'Launch School is the best!' repeatedly until a certain number of lines have been printed. The program should obtain the number of lines from the user, and should insist that at least 3 lines are printed.
+
+loop do
+  puts ">> How many output lines do you want? Please enter a number >= 3."
+  lines = gets.chomp.to_i
+  if lines >= 3
+    lines.times { puts "Launch School is the best!" }
+    break
+  else puts "Please try again with a number greater than 3."
+  end
+end
+
+# 6. Write a program that displays a welcome message, but only after the user enters the correct password, where the password is a string that is defined as a constant in your program. Keep asking for the password until the user enters the correct password.
+
+PASSWORD = "launchschool123"
+
+loop do
+  puts ">> Please enter password:"
+  pass = gets.chomp
+  if pass == PASSWORD
+    puts "Welcome back, launch school student!"
+    break 
+  end
+end
+
+# 7. In the previous exercise, you wrote a program to solicit a password. In this exercise, you should modify the program so it also requires a user name. The program should solicit both the user name and the password, then validate both, and issue a generic error message if one or both are incorrect; the error message should not tell the user which item is incorrect.
+
+USERNAME = "jenesis"
+PASSWORD = "launchschool123"
+
+loop do
+  puts ">> Please enter user name:"
+  user = gets.chomp
+
+  puts " >> Please enter password:"
+  pass = gets.chomp
+
+  if user == USERNAME && pass == PASSWORD
+    puts "Welcome back, launch school student!"
+    break 
+  else puts "Authorization failed! Try again"
+  end
+end
+
